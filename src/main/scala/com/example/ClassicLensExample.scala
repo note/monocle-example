2c7d3f189a2c80b709386bd8f7554858d968b2ef
Michal Sitko <pierwszy1@gmail.com>
2017-05-23 15:24:40 +0200

    refactoring

--- src/main/scala/com/example/ClassicLensExample.scala
+++ src/main/scala/com/example/ClassicLensExample.scala
@@ -1,11 +1,20 @@
 package com.example
 
-import com.example.common.{Address, Person, Street}
 import monocle.Lens
+
+case class Person(fullName: String, address: Address)
+case class Address(city: String, street: Street)
+case class Street(name: String, number: Int)
+
+object PersonLenses {
+  val addressLens = Lens.apply[Person, Address](person => person.address)(newAddress => person => person.copy(address = newAddress))
+  val streetLens = Lens.apply[Address, Street](address => address.street)(newStreet => address => address.copy(street = newStreet))
+  val nameLens = Lens.apply[Street, String](street => street.name)(newName => street => street.copy(name = newName))
+}
 
 object ClassicLensExample {
   // to have access to lenses
-  import com.example.common.PersonLenses._
+  import PersonLenses._
 
   def main(args: Array[String]): Unit = {
    val bob = Person("Bob Dylan", Address("New York", Street("some", 67)))
