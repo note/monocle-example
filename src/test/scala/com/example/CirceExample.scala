a31fd3cebb4f32d763f6acd7909dbc7e37f3a8df
Michal Sitko <pierwszy1@gmail.com>
2017-05-23 16:52:13 +0200

    Circe Example enhanced

--- src/test/scala/com/example/CirceExample.scala
+++ src/test/scala/com/example/CirceExample.scala
@@ -8,33 +8,47 @@ class CirceExample extends WordSpec with Matchers {
   val street: JsonPath = root.order.address.street
 
   "JsonPath" should {
-    "work" in new Context {
-      val modified = street.string.modify(_.toUpperCase)(referenceJson("abc"))
-      modified should equal(referenceJson("ABC"))
+    "should work as non-optics equivalent" in {
+      val input = referenceJson("abc")
+      val withOptics = modifyWithOptics(input)
+      val withoutOptics = modifyWithoutOptics(input)
+
+      withOptics should equal(withoutOptics)
+      withOptics should equal(referenceJson("ABC"))
     }
   }
-}
 
-trait Context {
+  // let's capitalize street name without optics
+  def modifyWithoutOptics(json: io.circe.Json): io.circe.Json =
+    json.hcursor.
+      downField("order").
+      downField("address").
+      downField("street").
+      withFocus(_.mapString(_.toUpperCase)).top.get
+
+  // now with optics:
+  def modifyWithOptics(json: io.circe.Json): io.circe.Json =
+    street.string.modify(_.toUpperCase)(json)
+
   def referenceJson(streetName: String) =
     io.circe.parser.parse(s"""
-      |{
-      |  "order": {
-      |    "address": {
-      |      "street": "$streetName",
-      |      "city": "someCity"
-      |    },
-      |    "items": [
-      |      {
-      |        "name": "OK Computer",
-      |        "amount": 1
-      |      },
-      |      {
-      |        "name": "Kid A",
-      |        "amount": 3
-      |      }
-      |    ]
-      |  }
-      |}
+                             |{
+                             |  "order": {
+                             |    "address": {
+                             |      "street": "$streetName",
+                             |      "city": "someCity"
+                             |    },
+                             |    "items": [
+                             |      {
+                             |        "name": "OK Computer",
+                             |        "amount": 1
+                             |      },
+                             |      {
+                             |        "name": "Kid A",
+                             |        "amount": 3
+                             |      }
+                             |    ]
+                             |  }
+                             |}
     """.stripMargin).right.get
 }
