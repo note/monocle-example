1b84c52ed455b42a5676a66cd18fb88480fcaad9
Michal Sitko <pierwszy1@gmail.com>
2017-06-13 13:28:27 +0200

    add OptionalExample

--- build.sbt
+++ build.sbt
@@ -4,14 +4,15 @@ version := "1.0"
 
 scalaVersion := "2.12.2"
 
-val monocleVersion = "1.4.0"
+val monocleVersion  = "1.4.0"
+val circeVersion    = "0.8.0"
 
 libraryDependencies ++= Seq(
   "com.github.julien-truffaut" %% "monocle-core"  % monocleVersion,
   "com.github.julien-truffaut" %% "monocle-macro" % monocleVersion,
-  "io.circe"                   %% "circe-core"    % "0.8.0",
-  "io.circe"                   %% "circe-parser"  % "0.8.0",
-  "io.circe"                   %% "circe-optics"  % "0.8.0",
+  "io.circe"                   %% "circe-core"    % circeVersion,
+  "io.circe"                   %% "circe-parser"  % circeVersion,
+  "io.circe"                   %% "circe-optics"  % circeVersion,
   "com.github.julien-truffaut" %% "monocle-law"   % monocleVersion % "test",
-  "org.scalatest"              %% "scalatest"     % "3.0.1"        % "test"
+  "org.scalatest"              %% "scalatest"     % "3.0.3"        % "test"
 )
---
+++ src/test/scala/com/example/OptionalExample.scala
@@ -0,0 +1,75 @@
+package com.example
+
+import monocle._
+import org.scalatest.{FlatSpec, Matchers}
+
+import scalaz.{Applicative, \/}
+
+sealed trait OperationError
+
+case class ErrorA(message: String, details: DetailedErrorA) extends OperationError
+case object ErrorB extends OperationError
+
+case class DetailedErrorA(detailedMessage: String)
+
+object ErrorOptics {
+  // That's straighforward approach, not recommended but shows the essence of Optional:
+  val detailedErrorA = Optional[OperationError, String]{
+    case err: ErrorA => Some(err.details.detailedMessage)
+    case _ => None
+  }{ newDetailedMsg => from =>
+    from match {
+      case err: ErrorA => err.copy(details = err.details.copy(newDetailedMsg))
+      case _ => from
+    }
+  }
+
+  // Better approach is to get `Optional[OperationError, String]` by composing Prism and Lens:
+  val errorA = Prism.partial[OperationError, ErrorA] {
+    case err: ErrorA => err
+  }(identity)
+
+  val detailedError =
+    Lens[ErrorA, DetailedErrorA](_.details)(newDetails => from => from.copy(details = newDetails))
+
+  val detailedErrorMsg =
+    Lens[DetailedErrorA, String](_.detailedMessage)(newMsg => from => from.copy(detailedMessage = newMsg))
+
+  val composedDetailedErrorA: Optional[OperationError, String] =
+    errorA.composeLens(detailedError.composeLens(detailedErrorMsg))
+}
+
+class OptionalExample extends FlatSpec with Matchers {
+  import ErrorOptics._
+
+  val examples = List(
+    Example(ErrorA("msg", DetailedErrorA("detailedMessage")), Some(ErrorA("msg", DetailedErrorA("DETAILEDMESSAGE")))),
+    Example(ErrorB, None)
+  )
+
+  "detailedErrorA Optional" should "work as expected" in {
+    val modify = detailedErrorA.modifyOption(_.toUpperCase)
+
+    testExamples { example =>
+      modify(example.input) should equal(example.expectedOutput)
+    }(examples)
+  }
+
+  "composedDetailedErrorA Optional" should "work as expected" in {
+    val modify = detailedErrorA.modifyOption(_.toUpperCase)
+
+    testExamples { example =>
+      modify(example.input) should equal(example.expectedOutput)
+    }(examples)
+  }
+
+  def testExamples(operationToTest: Example => Unit)(examples: Seq[Example]) = {
+    examples.foreach { example =>
+      operationToTest(example)
+    }
+  }
+
+}
+
+case class Example(input: OperationError, expectedOutput: Option[OperationError])
+
